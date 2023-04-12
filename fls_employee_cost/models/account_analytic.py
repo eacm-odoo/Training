4dda44cab6068d8ddb8eea8eb31ca73333cd8845
zna <zna@odoo.com>
2023-04-12 10:56:48 -0700

    [FIX] Change calculated conversion on timesheet line to line company

--- fls_employee_cost/models/account_analytic.py
+++ fls_employee_cost/models/account_analytic.py
@@ -7,15 +7,10 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     def _timesheet_postprocess_values(self, values):
-        """ Get the addionnal values to write on record
-            :param dict values: values for the model's fields, as a dictionary::
-                {'field_name': field_value, ...}
-            :return: a dictionary mapping each record id to its corresponding
-                dictionary values to write (may be empty).
-        """
         result = {id_: {} for id_ in self.ids}
         sudo_self = self.sudo()  # this creates only one env for all operation that required sudo()
         # (re)compute the amount (depending on unit_amount, employee_id for the cost, and account_id for currency)
+        ##### CUSTOM CODE START #####
         if any(field_name in values for field_name in ['unit_amount', 'employee_id', 'account_id', 'is_adjusted']):
             for timesheet in sudo_self:
                 cost = timesheet._hourly_cost()
@@ -23,7 +18,8 @@ class AccountAnalyticLine(models.Model):
                 if values.get('is_adjusted') or (not values.get('is_adjusted') and self.is_adjusted):
                     amount = 0
                 amount_converted = timesheet.employee_id.currency_id._convert(
-                    amount, timesheet.account_id.currency_id or timesheet.currency_id, self.env.company, timesheet.date)
+                    amount, timesheet.account_id.currency_id or timesheet.currency_id, timesheet.company_id, timesheet.date)
+                #####  CUSTOM CODE END  #####
                 result[timesheet.id].update({
                     'amount': amount_converted,
                 })
