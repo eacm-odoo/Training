87aa2e39eae4497ab700b0292673fafe57c004aa
laga-odoo <laga@odoo.com>
2023-08-21 13:22:18 -0400

    [ADD] update_currencies server action

--- fls_employee_cost/models/account_move.py
+++ fls_employee_cost/models/account_move.py
@@ -1,11 +1,18 @@
-from odoo import models, api, fields
+from odoo import models, api
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-    
-    def update_currencies(self):
+
+    def _update_currency_lines(self):
         USD = self.env['res.currency'].search([('name','=','USD')])
-        for line in self.line_ids.filtered(lambda l: l.product_id.invoice_policy in ['delivery', 'delivered_manual']):
+        for line in self.line_ids.filtered(lambda l: l.product_id.invoice_policy in ['delivered_manual', 'delivered_milestones']):
             line.credit = line.currency_id._convert(line.credit, USD, line.company_id, line.date)
 
+    def update_currencies(self):
+        self._update_currency_lines()
+
+    @api.model
+    def action_update_currencies(self):
+        for move in self:
+            move._update_currency_lines()
