54bd7c5f0e1b42dc5ecfa807de60bd5975d6fe38
zna <zna@odoo.com>
2023-09-13 15:16:08 -0700

    [ADD] Purchase Order automated actions

---
+++ fls_purchase/models/account_analytic.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = "account.analytic.line"
+
+    recent_contract_id = fields.Many2one("hr.contract", compute="_compute_contract_id", store="True", string="Employee Contract")
+
+    @api.depends("employee_id", "employee_id.contract_ids")
+    def _compute_contract_id(self):
+        for line in self:
+            line.recent_contract_id = False
+            for contract in line.employee_id.contract_ids:
+                if contract.state == "open" and line.recent_contract_id != False and contract.id > line.recent_contract_id.id:
+                    line.recent_contract_id = contract.id
+
