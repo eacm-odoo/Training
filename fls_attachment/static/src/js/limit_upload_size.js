6fb78ed929f907cf13758fdc75c98ac99d04a0a3
laga-odoo <laga@odoo.com>
2023-09-22 10:32:30 -0400

    [MODF] proper ajax rpc | call to super

--- fls_attachment/static/src/js/limit_upload_size.js
+++ fls_attachment/static/src/js/limit_upload_size.js
@@ -1,10 +1,11 @@
 /** @odoo-module **/
 
 import { registerPatch } from '@mail/model/model_core';
-import { attr } from '@mail/model/model_field';
-import Dialog from 'web.Dialog';
 
+import Dialog from 'web.Dialog';
 import core from 'web.core';
+import ajax from 'web.ajax';
+
 const _t = core._t;
 
 registerPatch({
@@ -14,29 +15,37 @@ registerPatch({
         * @override
         */
         async _performUpload({ files }) {
-            console.log('CUSTOM FILE UPLOADER!!!');
+            console.log('entering _performUpload override');
 
-            const attachmentLimit = await this.async(() => this.env.services.rpc({
-                model: 'ir.config_parameter',
-                method: 'get_param',
-                args: ['fls_attachment.attachment_limit'],
-            }));
+            try {
+                const attachmentLimit = await ajax.rpc("/web/dataset/call_kw/ir.config_parameter/get_param", {
+                    model: 'ir.config_parameter',
+                    method: 'get_param',
+                    args: ['fls_attachment.attachment_limit'],
+                    kwargs: {},
+                });
+                console.log('attachmentLimit:', attachmentLimit);
 
-            const limit = parseFloat(attachmentLimit) * 1024 ** 2; // converting to bytes
-            let filesWithinLimit = [];
-
-            for (const file of files) {
-                if (limit && file.size > limit) {
-                    Dialog.alert(this, _t('The file size exceeds the maximum limit of ${attachmentLimit} MB.'), {
-                        title: 'Uploading Size Restriction',
-                    });
-                } else {
-                    filesWithinLimit.push(file);
+                const limit = parseFloat(attachmentLimit) * 1024 ** 2; // converting to bytes
+                let filesWithinLimit = [];
+                var i = 0;
+                for (const file of files) {
+                    console.log('file.size:', file.size, i);
+                    i++;
+                    if (limit && file.size > limit) {
+                        Dialog.alert(this, _t(`The file size exceeds the maximum limit of ${attachmentLimit} MB.`), {
+                            title: _t('Uploading Size Restriction'),
+                        });                        
+                    } else {
+                        filesWithinLimit.push(file);
+                    }
                 }
-            }
-
-            if (filesWithinLimit.length) {
-                await this._super._performUpload({ files: filesWithinLimit });
+                console.log('filesWithinLimit.length:', filesWithinLimit.length);
+                if (filesWithinLimit.length) {
+                    return this._super({ files: filesWithinLimit });
+                }
+            } catch (error) {
+                console.error('Error fetching attachment limit:', error);
             }
         },
     }
