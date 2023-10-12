8330ac156ed2de902f99203198140087a770a703
kusn-odoo <kusn@odoo.com>
2023-10-12 19:27:56 -0400

    Add vendor bill automate template

--- fls_purchase/models/account_move.py
+++ fls_purchase/models/account_move.py
@@ -7,6 +7,8 @@ class AccountMove(models.Model):
 
     date = fields.Date(compute="_compute_accounting_date", store=True)
     formatted_amount_total = fields.Char("Formatted Amount Total")
+    vendor_company_name = fields.Char("Vendor Company Name")
+
     @api.depends("line_ids")
     def _compute_accounting_date(self):
         for mv in self:
@@ -20,20 +22,23 @@ class AccountMove(models.Model):
     @api.model
     def create(self, vals):
         move = super(AccountMove, self).create(vals)
-        move_type = vals.get('move_type')
-        if move_type == 'in_invoice' and not move.partner_id.is_company:
-            template = self.env.ref('fls_purchase.mail_template_vendor_bill_approval', raise_if_not_found=False)
-            if template:
-                
-                move.formatted_amount_total ="{:,}".format(5000000)
-                # data = move._context.copy()
-                # pdf = self.env["ir.actions.report"].sudo()._render_qweb_pdf('account.report_invoice_with_payments',res_ids=move.ids, data=data)
-                # attachment_ids=[(0, 0, {
-                #     'name': 'vendor_bill.pdf',
-                #     'datas': pdf.encode('base64'),
-                #     'type': 'binary',
-                # })]
-                # self.with_context(is_reminder=True).message_post_with_template(template.id, email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature", composition_mode='comment',res_id=move.id,attachment_ids=attachment_ids)
-                self.with_context(is_reminder=True).message_post_with_template(template.id, email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature", composition_mode='comment',res_id=move.id)
+
+        if move.move_type == 'in_invoice' and not move.partner_id.is_company and move.partner_id.vendor:
+            employee_record = self.env['hr.employee'].search([('related_contact_ids', 'in',[move.partner_id.id] )],limit=1)
+            if employee_record:
+                move.vendor_company_name = employee_record.company_id.name
+                template = self.env.ref('fls_purchase.mail_template_vendor_bill_approval', raise_if_not_found=False)
+                if template:
+                    
+                    move.formatted_amount_total ="{:,}".format(move.amount_total)
+                    # data = move._context.copy()
+                    # pdf = self.env["ir.actions.report"].sudo()._render_qweb_pdf('account.report_invoice_with_payments',res_ids=move.ids, data=data)
+                    # attachment_ids=[(0, 0, {
+                    #     'name': 'vendor_bill.pdf',
+                    #     'datas': pdf.encode('base64'),
+                    #     'type': 'binary',
+                    # })]
+                    # self.with_context(is_reminder=True).message_post_with_template(template.id, email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature", composition_mode='comment',res_id=move.id,attachment_ids=attachment_ids)
+                    self.with_context(is_reminder=True).message_post_with_template(template.id, email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature", composition_mode='comment',res_id=move.id)
 
         return move
