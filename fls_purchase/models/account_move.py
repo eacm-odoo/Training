26d9b83f3aeb356a8f4e04ecd1b364d03ea35134
kusn-odoo <kusn@odoo.com>
2023-10-12 14:57:43 -0400

    [ADD] 307 Vendor Bills email on Create

--- fls_purchase/models/account_move.py
+++ fls_purchase/models/account_move.py
@@ -6,7 +6,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     date = fields.Date(compute="_compute_accounting_date", store=True)
-
+    formatted_amount_total = fields.Char("Formatted Amount Total")
     @api.depends("line_ids")
     def _compute_accounting_date(self):
         for mv in self:
@@ -17,3 +17,25 @@ class AccountMove(models.Model):
                 if mv.move_type == "in_invoice" and purchase_id and date_planned:
                     mv.date = date_planned
 
+    @api.model
+    def create(self, vals):
+        move = super(AccountMove, self).create(vals)
+        move_type = vals.get('move_type')
+        if move_type == 'in_invoice' and not move.partner_id.is_company:
+            template = self.env.ref('fls_purchase.mail_template_vendor_bill_approval', raise_if_not_found=False)
+            if template:
+                import locale
+                user_locale = self.env.context.get('lang', 'en_US')
+                locale.setlocale(locale.LC_ALL, user_locale)
+                move.formatted_amount_total = locale.currency(move.amount_total, grouping=True)
+                # data = move._context.copy()
+                # pdf = self.env["ir.actions.report"].sudo()._render_qweb_pdf('account.report_invoice_with_payments',res_ids=move.ids, data=data)
+                # attachment_ids=[(0, 0, {
+                #     'name': 'vendor_bill.pdf',
+                #     'datas': pdf.encode('base64'),
+                #     'type': 'binary',
+                # })]
+                # self.with_context(is_reminder=True).message_post_with_template(template.id, email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature", composition_mode='comment',res_id=move.id,attachment_ids=attachment_ids)
+                self.with_context(is_reminder=True).message_post_with_template(template.id, email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature", composition_mode='comment',res_id=move.id)
+
+        return move
