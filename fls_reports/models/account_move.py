33a2a0fa15db43fe239e97c00d9a8d90d7fbe94d
kusn-odoo <kusn@odoo.com>
2023-10-27 15:58:21 -0400

    [Fix] Reports issues

--- fls_reports/models/account_move.py
+++ fls_reports/models/account_move.py
@@ -1,19 +1,19 @@
-from odoo import models,fields
+from odoo import models,fields,api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    client_order_ref = fields.Char(compute = 'compute_sale_order_id')
-    reference = fields.Char(compute = 'compute_sale_order_id')
 
+    client_order_ref = fields.Char("Client Order Reference")
+    reference = fields.Char("Reference")
     bank_acount_id = fields.Many2one(comodel_name='res.partner.bank')
     analytic_account_to_invoice = fields.Boolean(string= 'Analytic Account on Report?')
     currency_name = fields.Char(related = 'currency_id.name')
-    def compute_sale_order_id(self):
-        for record in self:
-            if record.sale_order_count>0:
-                record.client_order_ref = record.line_ids.sale_line_ids[0].order_id.client_order_ref
-                record.reference = record.line_ids.sale_line_ids[0].order_id.ref
-            else:
-                record.client_order_ref = ""
-                record.reference = ""
+    
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        if res.sale_order_count>0:
+            res.client_order_ref = res.line_ids.sale_line_ids[0].order_id.client_order_ref
+            res.reference = res.line_ids.sale_line_ids[0].order_id.ref
+        return res
