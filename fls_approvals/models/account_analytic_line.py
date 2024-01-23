2f0ad862c2977294fa461b34077d6f4855ef0342
laga-odoo <laga@odoo.com>
2024-01-22 20:50:53 -0500

    [IMP] aal compute override filter

--- fls_approvals/models/account_analytic_line.py
+++ fls_approvals/models/account_analytic_line.py
@@ -8,8 +8,6 @@ class AccountAnalyticLine(models.Model):
 
     @api.depends('task_id.sale_line_id', 'project_id.sale_line_id', 'employee_id', 'project_id.allow_billable')
     def _compute_so_line(self):
-        for timesheet in self.filtered(lambda t: not t.is_so_line_edited and t._is_not_billed()):
-            if timesheet.prevent_so_line_update:
-                continue
+        for timesheet in self.filtered(lambda t: not t.is_so_line_edited and t._is_not_billed() and not t.prevent_so_line_update):
             timesheet.so_line = timesheet.project_id.allow_billable and timesheet._timesheet_determine_sale_line()
 
