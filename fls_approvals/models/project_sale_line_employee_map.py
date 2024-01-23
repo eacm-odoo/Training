574066569a58633f9b179d257d33fee0902e2106
laga-odoo <laga@odoo.com>
2024-01-22 20:28:05 -0500

    [FIX] super call logic

--- fls_approvals/models/project_sale_line_employee_map.py
+++ fls_approvals/models/project_sale_line_employee_map.py
@@ -5,30 +5,30 @@ class ProjectProductEmployeeMap(models.Model):
     _inherit = 'project.sale.line.employee.map'
 
     def write(self, values):
-        result = super(ProjectProductEmployeeMap, self).write(values)
         if 'sale_line_id' in values:
-            analytic_lines_to_preserve = self._get_validated_lines_to_preserve()
+            analytic_lines_to_preserve = self._get_validated_analytic_lines()
+            result = super(ProjectProductEmployeeMap, self).write(values)
             self._restore_preceding_sol_id(analytic_lines_to_preserve)
-        return result
+            return result
+        else:
+            return super(ProjectProductEmployeeMap, self).write(values)
 
-    def _get_validated_lines_to_preserve(self):
+    def _get_validated_analytic_lines(self):
         '''Builds a map of validated analytic lines to their associated sale order line ids.'''
         analytic_lines_to_preserve = {}
         for map_entry in self:
-            validated_analytic_lines = self.env['account.analytic.line'].search([
+            validated_lines = self.env['account.analytic.line'].search([
                 ('project_id', '=', map_entry.project_id.id),
                 ('user_id', '=', map_entry.project_id.user_id.id),
                 ('validated', '=', True)
             ])
-            for line in validated_analytic_lines:
-                analytic_lines_to_preserve[line.id] = line.so_line.id
+            analytic_lines_to_preserve.update({line.id: line.so_line.id for line in validated_lines})
         return analytic_lines_to_preserve
 
     def _restore_preceding_sol_id(self, analytic_lines_to_preserve):
         '''Restores the sale order line id for validated analytic lines; never updated ex post facto.'''
         for line_id, old_so_line_id in analytic_lines_to_preserve.items():
             line = self.env['account.analytic.line'].browse(line_id)
-            if line.exists() and line.so_line.id != old_so_line_id:
-                line.write({'prevent_so_line_update': True})
+            if line and line.so_line.id != old_so_line_id:
+                line.prevent_so_line_update = True
                 line.sudo().write({'so_line': old_so_line_id})
-
