845eb07704a9a7cba1f2c7523af25b8b719d7e46
laga-odoo <laga@odoo.com>
2024-02-28 12:51:36 -0500

    [ADD] _insert_followers context check

--- fls_notifications/models/mail_followers.py
+++ fls_notifications/models/mail_followers.py
@@ -7,7 +7,7 @@ class MailFollowers(models.Model):
     def _insert_followers(self, res_model, res_ids,
                             partner_ids, subtypes=None,
                             customer_ids=None, check_existing=True, existing_policy='skip'):
-        if not partner_ids:
+        if not partner_ids and self.env.context.get('block_notifications'):
             return
         return super()._insert_followers(res_model, res_ids, 
                                          partner_ids, subtypes, 
