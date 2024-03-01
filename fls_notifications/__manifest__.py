327b467370612993e132d8c355b3b81d607e3b45
laga-odoo <laga@odoo.com>
2024-03-01 13:28:12 -0500

    [IMP] manifest

--- fls_notifications/__manifest__.py
+++ fls_notifications/__manifest__.py
@@ -1,18 +1,15 @@
 {
     'name': 'Block User Assignment Notifications',
+    'version': '1.0',
+    'category': 'Customization',
     'summary': 'Blocks User Assignment Notifications and Emails from Being Sent.',
     'description': """
         This module blocks the sending of user assignment notifications and emails.
+        Task ID: 3724016
     """,
-    'author': 'Odoo Development Services',
-    'maintainer': 'Odoo Development Services',
+    'author': 'Odoo Inc',
     'website': 'https://www.odoo.com',
     'license': 'OPL-1',
-    'version': '1.0',
-    'task_ids': [3724016],
     'depends': ['mail', 'project'],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
 }
 
