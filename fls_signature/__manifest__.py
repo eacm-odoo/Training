3d3cdacce3bba7198a9b0652603f59512fd8ecf6
dipa-odoo <dipa@odoo.com>
2024-03-14 09:28:34 -0700

    [ADD] Draft version of the module ready for UAT

--- fls_signature/__manifest__.py
+++ fls_signature/__manifest__.py
@@ -2,13 +2,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'FLS: Smart Employee Contract Signature',
-    'version': '1.1',
+    'version': '1.0',
     'category': 'Customization',
     'description': """
         Task ID: 3724021
         Quadgram: dipa
 
         [REF:322] Implement Employee Contract Signature in Odoo
+
+        - Dynamic Auto-fill for Employee Contract Signature
+        - add the signed document to employee page
+
     """,
     'author': 'Odoo Inc',
     'website': 'http://www.odoo.com',
@@ -16,6 +20,8 @@
     'depends': ['sign', 'hr'],
     'data': [
         'views/sign_item_type_views.xml',
+        'views/hr_employee_views.xml',
+        'data/data.xml'
     ],
     'installable': True,
 }
--- fls_signature/models/res_partner.py
+++
@@ -1,4 +0,0 @@
-from odoo import models, fields, api, _
-
-class ResPartner(models.Model): 
-    _inherit = 'res.partner'   
